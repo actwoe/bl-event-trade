d2ac689c8e3ae0bb6d5b916fdf5caf13561b8fd1
chwonkim88 <chwonkim88@gmail.com>
2026-07-08 17:17:58 +0900

    푸터 문구 수정

--- components/shared/SiteFooter.tsx
+++ components/shared/SiteFooter.tsx
@@ -1,11 +1,11 @@
 export function SiteFooter() {
   return (
     <footer className="bg-neutral-100 px-4 pb-6 pt-0 text-center">
-      <p className="text-[11px] font-black text-neutral-500">
-        제작 NP @ru1ned1over
-      </p>
       <p className="mt-1 text-[10px] font-bold leading-5 text-neutral-400">
         사이트에 업로드된 모든 이미지의 저작권은 각 플랫폼과 작가님께 있습니다.
+      </p>
+            <p className="text-[11px] font-black text-neutral-500">
+        제작 NP @ru1ned1over
       </p>
     </footer>
   );
